dcaeb9eddc16488993ed3fb361d745de4bc5c5c2
Matheus Amaral De Camargo Cardoso Lima <41813664803@info.local>
2019-12-06 11:08:25 -0300

    06/12

---
+++ OpFlix/src/Pages/Mapa/Mapa.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import logo from    '../../img/logo.png';
+import {Link} from 'react-router-dom';
+import './Mapa.css';
+
+class Mapa extends Component{
+
+    
+
+
+    render(){
+        return(
+            <div className="divona" id="map">
+                
+            </div>
+        )
+    }
+}
+
+export default Mapa;
